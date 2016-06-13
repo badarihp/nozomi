c4a85448e100b6c36ace06d952ae23d440379419
Philip Jameson <pjameson@pjameson.us>
2016-06-13 07:06:00 -0700

    Still really hacky, but parsing single frames now

---
+++ test/StringUtils.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "src/StringUtils.h"
+
+TEST(StringUtilsTest, throws_if_offset_is_too_large) {
+  ASSERT_FALSE(true);
+}
+
+TEST(StringUtilsTest, handles_offset_larger_than_one_buf_in_the_chain) {
+  ASSERT_FALSE(true);
+}
+
+TEST(StringUtilsTest, defaults_to_zero_offset) {
+  ASSERT_FALSE(true);
+}
+
+TEST(StringUtilsTest, handles_single_buffer_with_offset) {
+  ASSERT_FALSE(true);
+}
